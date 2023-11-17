1a17437e6ca07ee6a740b962a83f264b91dacd20
Arshaq Ahmed <arshahm2004@gmail.com>
2023-11-17 22:56:39 +0000

    initial code

--- minmax.cpp
+++ minmax.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 int main() {
-    l
-
+    vector<int> nums;
+    cout << "Enter an integer: ";
+    
+    for (int i = 0; i < 10; i++) {
+        int randomints = i % 101;
+        nums.push_back(randomints);
+        cout << randomints << " ";
+    }
+    
     return 0;
 }
